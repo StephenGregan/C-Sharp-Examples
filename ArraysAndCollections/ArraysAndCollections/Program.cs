550e8868c0badd08432da28491809b8e3c632188
StephenGregan <grego@live.ie>
2018-08-02 11:08:56 +0100

    initial commit

---
+++ ArraysAndCollections/ArraysAndCollections/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] someIntergers = new int[3];
+            Console.WriteLine(someIntergers[0]);
+            Console.WriteLine(someIntergers[1]);
+            Console.WriteLine(someIntergers[2]);
+            Console.ReadLine();
+
+            int[] moreIntergers = new int[] {1, 2, 3, 4, 5};
+            Console.WriteLine(moreIntergers[0]);
+            Console.WriteLine(moreIntergers[1]);
+            Console.WriteLine(moreIntergers[2]);
+            Console.WriteLine(moreIntergers[3]);
+            Console.WriteLine(moreIntergers[4]);
+            Console.ReadLine();
+        }
+    }
+}
